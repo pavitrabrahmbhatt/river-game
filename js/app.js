0958253702a3997d9039f355b8c7657e47c0620b
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-07-08 17:29:28 -0500

    end of class

--- js/app.js
+++ js/app.js
@@ -1,39 +1,60 @@
 console.log("River Boat Game");
 
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+
+
+
 
 
 const boat = {
 	lives: 3,
 	name: null,
-	direction: ''
-},
+	direction: '',
+	x: 400,
+    y: 40,
+    isAlive: false,
 
-move: function(){
 
-    	if(this.direction === 'left'){
-    		if(this.y + 20 < 800){
-    		    // move left along y axis
-                this.y += 10;
+// start: function () {
+
+// }
+
+
+move: function() {
+
+    	if(this.direction === "left"){
+    		if(this.x - 350 > 0){
+    		    //move left on x axis
+                this.x -= 10;
     		}
     	}
+
     	else if(this.direction === "right"){
-            if(this.y >= 20)
-            // move right from y axis
-    		this.y -= 10;
-
-    	}else if(this.direction === "down"){
-    	    //set to 200px so cant move above dark area
-    	    if(this.x >= screenBorder + 20)
-    	        this.x -= 10;
-
-    	}else if(this.direction === "up"){
-    	    //set to only move within dark grey area
-    	    if(this.x <= canvas.height - screenBorder - 80) {
-                this.x += 10;
-            }
+            if(this.x + 350 < 800){
+            // move right on x axis
+    		this.x += 10;
+    	    }
     	}
+
+
+
+
+    	
     }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
