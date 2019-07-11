aef649be9972aaedf9926284346be88e02e663c5
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-07-11 15:46:36 -0500

    completed game

--- js/app.js
+++ js/app.js
@@ -1,33 +1,32 @@
+
+
 console.log("River Boat Game");
 
 const canvas = document.getElementById('my-canvas');
-console.log(canvas) // cool now we have the canvas
-
-// the "context" is what you actually draw on -- you basically always need it
+console.log(canvas);
 const ctx = canvas.getContext('2d');
-console.log(ctx); // cool, our rendering context is set up
+console.log(ctx); 
 
-//$("#gif").hide()
 $("#my-canvas").hide()
 $("#submit").hide()
 $("#name").hide()
 $("#gameOver").hide()
 $("#sailBoat").hide()
 $("#gameWon").hide()
+$("#instructions").hide()
+$("#shark").hide()
 
-
-
+//class for the main player
 class Boat {
 	constructor (boatName) {
 		this.name = boatName;
 		this.lives = 5;
-		this.x = 400 
-		this.y = 700 
-		this.r = 20
-		this.speed = 10
-	   	
+		this.x = 400;
+		this.y = 700; 
+		this.r = 20;
+		this.speed = 10;   	
 	}
-	move(direction) {
+	move(direction) { //move player 
 	    if(direction=="ArrowDown") {
 	      this.y += this.speed;
 	    }
@@ -42,29 +41,21 @@ class Boat {
 	    }
 	    game.clearCanvas();
 	    this.draw();
-	    game.countLives()
-	    game.checkCollision()
-	    game.countLives()
-	    game.isAlive()
-	    game.checkWin()
+	    game.countLives();
+	    game.checkCollision();
+	    game.countLives();
+	    game.isAlive();
+	    game.checkWin();
 	    ctx.font = "30px Arial";
 	    ctx.fillStyle = 'black';
-		ctx.fillText("Lives: " + game.player.lives, 10, 780);
-		
-		
+		ctx.fillText("Lives: " + game.player.lives, 10, 780); //print the lives	
 	}
-	
 	draw() {
-		let sailBoat = $("#sailBoat")
-		
+		let sailBoat = $("#sailBoat");
 		ctx.drawImage(sailBoat[0], this.x, this.y, 50,50);
-	  	// ctx.beginPath();
-	   //  ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
-	   //  ctx.fillStyle = "cadetblue";
-	   //  ctx.fill();
 	}
 }
-
+//class for the obstacles
 class Obstacle {
 	constructor(column,row) {
 		this.x = column  
@@ -92,74 +83,48 @@ class Obstacle {
   	draw5() {
  		let grass = $("#grass")
 		ctx.drawImage(grass[0], this.x, this.y, 50,50);
-  	}
-
-  	
+  	}	
 }
-  
-
-
+//game class
 const game = {
-
 	player: null,
 	obstacles: [],
 	isAlive: false,
 	numberOfObstacles: 30,
-
 	countLives: function (){
 		if (this.checkCollision() === true){
-
 			this.player.lives--;
 			console.log(this.player.lives);
-
 		}
 		ctx.font = "30px Arial";
 		ctx.fillStyle = 'black';
 		ctx.fillText("Lives: " + game.player.lives, 10, 780);
 		
 	},
-	checkWin(){
-		if (this.player.y < 10 ){
-			game.winGame()
-		}
-	},
-
 	isAlive: function(){
 		if (this.player.lives <= 0) {
-			this.endGame()
-			return false
+			this.endGame();
+			return false;
 		} else {
-			return true
+			return true;
 		}
 		ctx.font = "30px Arial";
 		ctx.fillStyle = 'black';
 		ctx.fillText("Lives: " + game.player.lives, 10, 780);
 	},
-
-	endGame: function () {
-			console.log("You have died");
-			stopAnimation()
-			game.clearCanvas()
-			$("#gameOver").show()
-		
-	},
-
 	makeBoat: function(boatName) {
-		const gameBoat = new Boat(boatName)
-		this.player = gameBoat
+		const gameBoat = new Boat(boatName);
+		this.player = gameBoat;
 		console.log(this.player);
 		$( "#nameTitle" ).text(this.player.name);
-		$("#name").hide()
-		$("#submit").hide()	
-		
-		this.player.draw()
-		animate()
+		$("#name").hide();
+		$("#submit").hide();
+		this.player.draw();
+		animate();
 	},
-
 	clearCanvas() {
-  		ctx.clearRect(0, 0, canvas.width, canvas.height)
+  		ctx.clearRect(0, 0, canvas.width, canvas.height);
 	},
-
 	makeObstacles: function () {
 		for (let i = 0; i < this.numberOfObstacles; i++) {
 			let column = Math.floor(Math.random() * 750) + 5 
@@ -192,27 +157,19 @@ const game = {
 			} else if (whatPart = 600) {
 				this.obstacles.push(firstObstacle)
 				firstObstacle.draw5()
-			}
-			
-			
+			}	
 		}
 	},
-
-	
 	drawObstacles() {
 		for (let i = 0; i < this.obstacles.length; i++){
-	    	this.obstacles[i].draw5()
-		}
-		
+	    	this.obstacles[i].draw5();
+		}	
 	},
 	moveObstacles() {
-	 
 		for (let i = 0; i < game.obstacles.length; i++){
 	    	game.obstacles[i].x++;
 		}
-
 	},
-	
 	moreObstacles() {
 		for (let i = 0; i < 2; i++) {
 			let row = Math.floor(Math.random() * 5) + 1
@@ -245,11 +202,7 @@ const game = {
 			} else if (whatPart = 600) {
 				this.obstacles.push(newOb)
 				newOb.draw5()
-			}
-			
-			
-			
-			
+			}	
 		}
 	},
 	checkCollision() { 
@@ -263,16 +216,30 @@ const game = {
   			) { 
 	     		console.log("collision");
 	     		this.obstacles.splice(i,1);
-	     		this.countLives()
-	     		this.isAlive()
-	         	return true
+	     		this.countLives();
+	     		this.isAlive();
+	         	return true;
 
 	    	} 
 		} 
-		// this.drawObstacles()
-		return false
+		return false;
 	},
-
+	endGame: function () {
+			console.log("You have died");
+			stopAnimation();
+			game.clearCanvas();
+			$("#gameOver").show();	
+	},
+	checkWin() {
+		if (this.player.y < 10 ) {
+			game.winGame();
+		}
+	},
+	winGame() {
+			stopAnimation();
+			game.clearCanvas();
+			$("#gameWon").show();	
+	},
 	restartGame() {
 		$("#gameOver").hide()
 		$("#gameWon").hide()
@@ -283,87 +250,61 @@ const game = {
     	game.countLives()
 		game.isAlive()
 		animate()
-	},
-
-	winGame() {
-		
-			stopAnimation()
-			game.clearCanvas()
-			$("#gameWon").show()
-		
 	}
 }
-
-
 
 let requestID;
 let animationRunning = false;
 let frame = 0
 function animate() {
-
 	animationRunning = true; 
-	
-
 	game.clearCanvas();
 	game.moveObstacles();
-	game.drawObstacles()
+	game.drawObstacles();
 	game.player.draw();
-	game.countLives()
-	game.isAlive()
-	game.checkWin()
-	
-
-	
-
+	game.countLives();
+	game.isAlive();
+	game.checkWin();
 	// every 60 frames
-	frame++
+	frame++;
 	if (frame%40 === 0) {
-		game.moreObstacles()
+		game.moreObstacles();
 	}
-
-
-	requestID =window.requestAnimationFrame(animate)
+	requestID =window.requestAnimationFrame(animate);
 }
 
 function stopAnimation() {
-  cancelAnimationFrame(requestID)
+  cancelAnimationFrame(requestID);
   animationRunning = false;
 }
 
-
 $("#introImage").on('click', function() {
   	$("#introImage").hide();
-  	$("#name").show()
-  	$("#submit").show()
-  	$("#my-canvas").show()
-  	
-
- 
+  	$("#name").show();
+  	$("#submit").show();
+  	$("#my-canvas").show();
+  	$("#instructions").show();
 });
-
 
 $("#submit").on('click', function() {
     let theValue = $("#name").val();
     game.makeBoat(theValue);
     game.makeObstacles();
-    game.countLives()
-	game.isAlive()
-
+    game.countLives();
+	game.isAlive();
+	$("#instructions").hide();
 })
 
-
 $(document).on('keydown', function(e) {
-	game.player.move(e.key)
+	game.player.move(e.key);
 });
 
 $("#gameOver").on('click', function() {
-    game.restartGame()
-
+    game.restartGame();
 })
 
 $("#gameWon").on('click', function() {
-    game.restartGame()
-
+    game.restartGame();
 })
 
 
